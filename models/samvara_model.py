6685db0d2ac6b3c4ff45e959ee6d4ae891a7baee
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-09-29 17:06:37 +0200

    Bugfix in samvara_model.py

--- models/samvara_model.py
+++ models/samvara_model.py
@@ -10,18 +10,18 @@ def build_samvara_model():
     material_input_text = layers.Input(shape=(100,), name='material_input_text')
     quantum_input = layers.Input(shape=(2,), name='quantum_input')
 
-    # Material model
+    # Material model (image and text processing)
     material_model = build_material_model()
     material_output = material_model([material_input_image, material_input_text])
 
-    # Immaterial model (quantum layers)
+    # Immaterial model (quantum processing)
     immaterial_model = build_immaterial_model()
     immaterial_output = immaterial_model([quantum_input, material_output])
 
     # Combine material and immaterial outputs
     combined_output = layers.Concatenate()([material_output, immaterial_output])
 
-    # Final output layer (classification, for example)
+    # Final output layer (softmax classification)
     final_output = layers.Dense(10, activation='softmax')(combined_output)
 
     # Build the Samvara model
