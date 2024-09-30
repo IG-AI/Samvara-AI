5340c50c60405a913475c94eb2a6f2d60117cf06
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-09-30 16:04:16 +0200

    Update samvara_model.py

--- models/samvara_model.py
+++ models/samvara_model.py
@@ -1,30 +1,29 @@
 # models/samvara_model.py
 
+from tensorflow.keras.layers import Input, Dense, Concatenate
+from tensorflow.keras.models import Model
 from models.material_layers import build_material_model
 from models.immaterial_layers import build_immaterial_model
-from tensorflow.keras import layers, models
 
 def build_samvara_model():
-    # Input placeholders
-    material_input_image = layers.Input(shape=(32, 32, 3), name='material_input_image')
-    material_input_text = layers.Input(shape=(100,), name='material_input_text')
-    quantum_input = layers.Input(shape=(2,), dtype='complex64', name='quantum_input')  # Complex input
-
-    # Material model (image and text processing)
+    # Material model (image and text inputs)
+    image_input = Input(shape=(32, 32, 3), name='image_input')
+    text_input = Input(shape=(100,), name='text_input')
     material_model = build_material_model()
-    material_output = material_model([material_input_image, material_input_text])
 
-    # Immaterial model (quantum processing)
+    material_output = material_model([image_input, text_input])
+
+    # Immaterial model (quantum input)
+    quantum_input = Input(shape=(2,), name='quantum_input')
     immaterial_model = build_immaterial_model()
-    immaterial_output = immaterial_model([quantum_input, material_output])
 
-    # Combine material and immaterial outputs
-    combined_output = layers.Concatenate()([material_output, immaterial_output])
+    # Concatenate material and immaterial outputs
+    immaterial_output = immaterial_model(quantum_input)
 
-    # Final output layer (softmax classification)
-    final_output = layers.Dense(10, activation='softmax')(combined_output)
+    combined_output = Concatenate()([material_output, immaterial_output])
 
-    # Build the Samvara model
-    model = models.Model(inputs=[material_input_image, material_input_text, quantum_input], outputs=final_output)
+    # Final dense layers for classification
+    output = Dense(10, activation='softmax')(combined_output)
 
+    model = Model(inputs=[image_input, text_input, quantum_input], outputs=output)
     return model
