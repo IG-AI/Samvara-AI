ca73a8812d5b508c690c0131780df1763a3a79be
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-09-30 23:05:29 +0200

    Update samvara_model.py

--- models/samvara_model.py
+++ models/samvara_model.py
@@ -1,6 +1,6 @@
 # models/samvara_model.py
 
-import tensorflow as tf  # Add this line to import TensorFlow
+import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Concatenate
 from tensorflow.keras.models import Model
 from models.material_layers import build_material_model
@@ -14,14 +14,15 @@ def build_samvara_model():
 
     material_output = material_model([image_input, text_input])
 
-    # Immaterial model (quantum input)
-    quantum_input = Input(shape=(2,), name='quantum_input')
+    # Immaterial model (real and imaginary quantum inputs)
+    real_input = Input(shape=(2,), name='real_input')
+    imaginary_input = Input(shape=(2,), name='imaginary_input')
     immaterial_model = build_immaterial_model()
 
-    immaterial_output = immaterial_model(quantum_input)
+    immaterial_output = immaterial_model([real_input, imaginary_input])
 
     # Convert immaterial_output to float32 to match material_output type
-    immaterial_output_real = tf.math.real(immaterial_output)  # tf is now defined
+    immaterial_output_real = tf.math.real(immaterial_output)
     immaterial_output_float = tf.cast(immaterial_output_real, dtype=tf.float32)
 
     # Concatenate material and immaterial outputs (now both float32)
@@ -30,5 +31,5 @@ def build_samvara_model():
     # Final dense layers for classification
     output = Dense(10, activation='softmax')(combined_output)
 
-    model = Model(inputs=[image_input, text_input, quantum_input], outputs=output)
+    model = Model(inputs=[image_input, text_input, real_input, imaginary_input], outputs=output)
     return model
