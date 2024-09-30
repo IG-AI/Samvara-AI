da5c974ec9504f00177ca6b4e39843007d0b74c8
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-10-01 00:13:51 +0200

    Update samvara_model.py

--- models/samvara_model.py
+++ models/samvara_model.py
@@ -11,22 +11,16 @@ class RealPartLayer(Layer):
         return inputs  # Removed real extraction; both parts are treated as float32.
 
 def build_samvara_model():
-    # Material model
+    # Material model (image and text inputs)
     image_input = Input(shape=(32, 32, 3), name='image_input')
     text_input = Input(shape=(100,), name='text_input')
     material_model = build_material_model()
+
     material_output = material_model([image_input, text_input])
 
-    # Immaterial model
-    real_input = Input(shape=(2,), name='real_input')
-    imaginary_input = Input(shape=(2,), name='imaginary_input')
-    immaterial_model = build_immaterial_model()
-    immaterial_output = immaterial_model([real_input, imaginary_input])
+    # Final dense layers for classification
+    output = Dense(10, activation='softmax')(material_output)
 
-    combined_output = Concatenate()([material_output, immaterial_output])
-
-    # Final dense layer
-    output = Dense(10, activation='softmax')(combined_output)
-
-    model = Model(inputs=[image_input, text_input, real_input, imaginary_input], outputs=output)
+    model = Model(inputs=[image_input, text_input], outputs=output)
     return model
+    
