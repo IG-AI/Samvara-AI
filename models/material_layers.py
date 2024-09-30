6208b9560874ef0a62758cd60e882ec8ea6b8b34
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-09-30 16:08:00 +0200

    Update material_layers.py

--- models/material_layers.py
+++ models/material_layers.py
@@ -1,39 +1,32 @@
 # models/material_layers.py
 
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras.layers import Dense, Input, Concatenate
+from tensorflow.keras.models import Model
 
 def build_material_model():
-    # Input layer for images and text
-    image_input = layers.Input(shape=(32, 32, 3), name='image_input')
-    text_input = layers.Input(shape=(100,), name='text_input')
+    # Image input (e.g., 32x32 images with 3 color channels)
+    image_input = Input(shape=(32, 32, 3), name='image_input', dtype='float32')
+    
+    # Text input (e.g., a sequence of integers representing words)
+    text_input = Input(shape=(100,), name='text_input', dtype='int32')
+    
+    # Image processing
+    image_flatten = tf.keras.layers.Flatten()(image_input)
+    image_dense = Dense(128, activation='relu', name='image_dense')(image_flatten)
+    
+    # Text processing (e.g., embedding + dense layer)
+    text_embed = tf.keras.layers.Embedding(input_dim=10000, output_dim=64, name='text_embedding')(text_input)
+    text_flatten = tf.keras.layers.Flatten()(text_embed)
+    text_dense = Dense(128, activation='relu', name='text_dense')(text_flatten)
 
-    # Image processing (Layer 1)
-    image_features = layers.Conv2D(32, (3, 3), activation='relu')(image_input)
-    image_features = layers.MaxPooling2D(pool_size=(2, 2))(image_features)
-    image_features = layers.Conv2D(64, (3, 3), activation='relu')(image_features)
-    image_features_flat = layers.Flatten()(image_features)
-
-    # Text processing (Layer 1)
-    embedding_layer = layers.Embedding(input_dim=10000, output_dim=128, input_length=100)(text_input)
-    text_features = layers.LSTM(128)(embedding_layer)
-
-    # Combine both image and text features (Layer 2)
-    combined_features = layers.Concatenate()([image_features_flat, text_features])
-    dense_layer = layers.Dense(512, activation='relu')(combined_features)
-
-    # Lateral connection between Layer 3 and Layer 4
-    dense_layer = layers.Dropout(0.5)(dense_layer)
-    contextual_layer = layers.Dense(256, activation='relu')(dense_layer)
-    lateral_combined = layers.Concatenate()([dense_layer, contextual_layer])
-    lateral_layer = layers.Dense(256, activation='relu')(lateral_combined)
-
-    # Decision making (Layer 5)
-    decision_layer = layers.Dense(128, activation='relu')(lateral_layer)
-
-    # The final output is not a softmax, it's an intermediate feature layer to be used elsewhere
-    output = layers.Dense(128, activation='relu')(decision_layer)
-
-    # Build the model
-    model = models.Model(inputs=[image_input, text_input], outputs=output)
-    return model
+    # Concatenating both image and text representations
+    material_concat = Concatenate(name='material_concat')([image_dense, text_dense])
+    
+    # Output layer for the material model
+    material_output = Dense(128, activation='relu', name='material_output')(material_concat)
+    
+    # Creating the material model
+    material_model = Model(inputs=[image_input, text_input], outputs=material_output, name='material_model')
+    
+    return material_model
