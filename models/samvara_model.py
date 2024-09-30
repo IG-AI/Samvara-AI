0739ba78765bea1b73c9b0bbbfb514877ed0bed4
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-09-30 13:53:12 +0200

    Bugfix in samvara_model.py

--- models/samvara_model.py
+++ models/samvara_model.py
@@ -3,13 +3,12 @@
 from models.material_layers import build_material_model
 from models.immaterial_layers import build_immaterial_model
 from tensorflow.keras import layers, models
-import tensorflow as tf
 
 def build_samvara_model():
     # Input placeholders
     material_input_image = layers.Input(shape=(32, 32, 3), name='material_input_image')
     material_input_text = layers.Input(shape=(100,), name='material_input_text')
-    quantum_input = layers.Input(shape=(2,), name='quantum_input', dtype=tf.complex64)  # Set quantum input to complex
+    quantum_input = layers.Input(shape=(2,), name='quantum_input')
 
     # Material model (image and text processing)
     material_model = build_material_model()
@@ -19,12 +18,8 @@ def build_samvara_model():
     immaterial_model = build_immaterial_model()
     immaterial_output = immaterial_model([quantum_input, material_output])
 
-    # Split real and imaginary parts from the immaterial output
-    real_part = tf.math.real(immaterial_output)
-    imag_part = tf.math.imag(immaterial_output)
-
-    # Combine material output with real and imaginary parts of immaterial output
-    combined_output = layers.Concatenate()([material_output, real_part, imag_part])
+    # Combine material and immaterial outputs
+    combined_output = layers.Concatenate()([material_output, immaterial_output])
 
     # Final output layer (softmax classification)
     final_output = layers.Dense(10, activation='softmax')(combined_output)
@@ -34,5 +29,5 @@ def build_samvara_model():
 
     # Compile the model
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+    
     return model
