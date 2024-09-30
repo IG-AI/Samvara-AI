3724d2081c2115a04bedfab804bc3588f14429f8
Daniel "G_A" Ågstrand <d.e.agstrand@gmail.com>
2024-09-30 13:57:06 +0200

    Bugfix in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import tensorflow as tf
-from samvara_model import build_samvara_model
+from models.samvara_model import build_samvara_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 # Set random seed for reproducibility
