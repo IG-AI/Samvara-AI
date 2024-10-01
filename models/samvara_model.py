35ef784e2b1586c12ca59c9b2990c6dde5a597d1
IG-AI <d.e.agstrand@gmail.com>
2024-10-01 09:26:42 +0200

    Updated Samvara model to split the training of the material and immaterial models

--- models/samvara_model.py
+++ models/samvara_model.py
@@ -6,11 +6,10 @@ from tensorflow.keras.models import Model
 from models.material_layers import build_material_model
 from models.immaterial_layers import build_immaterial_model
 
-class RealPartLayer(Layer):
-    def call(self, inputs):
-        return inputs  # Returning directly, no need to extract real part anymore.
-
-def build_samvara_model():
+def build_samvara_model(include_immaterial=True):
+    """
+    Build the Samvara model. If include_immaterial=False, only train material layers.
+    """
     # Material model (image and text inputs)
     image_input = Input(shape=(32, 32, 3), name='image_input')
     text_input = Input(shape=(100,), name='text_input')
@@ -18,18 +17,24 @@ def build_samvara_model():
 
     material_output = material_model([image_input, text_input])
 
-    # Immaterial model (quantum input)
-    real_input = Input(shape=(2,), name='real_input')
-    imaginary_input = Input(shape=(2,), name='imaginary_input')
-    immaterial_model = build_immaterial_model()
+    if include_immaterial:
+        # Immaterial model (quantum input)
+        real_input = Input(shape=(2,), name='real_input')
+        imaginary_input = Input(shape=(2,), name='imaginary_input')
+        immaterial_model = build_immaterial_model()
 
-    immaterial_output = immaterial_model([real_input, imaginary_input])
+        immaterial_output = immaterial_model([real_input, imaginary_input])
 
-    # Concatenate material and immaterial outputs
-    combined_output = Concatenate()([material_output, immaterial_output])
+        # Concatenate material and immaterial outputs
+        combined_output = Concatenate()([material_output, immaterial_output])
+    else:
+        # Only material output
+        combined_output = material_output
 
     # Final dense layer for classification
     output = Dense(10, activation='softmax')(combined_output)
 
-    model = Model(inputs=[image_input, text_input, real_input, imaginary_input], outputs=output)
-    return model
+    if include_immaterial:
+        return Model(inputs=[image_input, text_input, real_input, imaginary_input], outputs=output)
+    else:
+        return Model(inputs=[image_input, text_input], outputs=output)
